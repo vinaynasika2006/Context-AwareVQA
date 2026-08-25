9d90e1b1a9c45c92cfb42bce16d4d73fe3355e02
vinaynasika2006 <168324080+vinaynasika2006@users.noreply.github.com>
2026-08-25 13:23:28 +0530

    Refactor bias mitigation to use a dictionary

--- bias.py
+++ bias.py
@@ -1,6 +1,26 @@
 def mitigate_bias(answer):
-    biased_words = ["man", "woman", "black", "white", "rich", "poor"]
-    for word in biased_words:
-        if word in answer.lower():
-            answer = answer.replace(word, "person")
-    return answer
+
+    replacements = {
+        "man": "person",
+        "woman": "person",
+        "black": "person",
+        "white": "person",
+        "rich": "person",
+        "poor": "person"
+    }
+
+    result = answer
+
+    for word, replacement in replacements.items():
+
+        result = result.replace(
+            word,
+            replacement
+        )
+
+        result = result.replace(
+            word.capitalize(),
+            replacement
+        )
+
+    return result
